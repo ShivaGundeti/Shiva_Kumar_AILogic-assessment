6daf58ff6dd8c7b13b442f51b96e169b5af3fef6
Shiva Gundeti <98530199+ShivaGundeti@users.noreply.github.com>
2026-09-04 13:30:21 +0530

    Made changes in the Maxswap.js

--- MaxSwap.js
+++ MaxSwap.js
@@ -7,31 +7,73 @@ Input:
 
 num = 2736
 
- 
+
 
 Output:
 
 7236
 */
 
-function MaxSwap(number)
-{
-    let diff = 0;
-    let originalNumber = number;
-    let num = number.toString().split("");
+function swap(swapNumber) {
+let num = swapNumber.toString().split("");
     console.log(num);
     for(let i =0; i<2; i++)
     {
         [num[i],num[i+1]] = [num[i+1],num[i]]
     }
-    let newNumber = num.join("")
-    console.log(newNumber);
-    let minus = newNumber - originalNumber;
-    if(minus > diff)
+    return num
+}
+
+
+function MaxSwap(number)
+{
+    let hash = {}
+    let originalNumber = number;
+    var swapped = swap(originalNumber)
+    let newNumber = swapped.join("")
+    if(newNumber > originalNumber)
     {
-        diff = minus
+        hash[newNumber] = 1;
     }
+    else{
+        for(let i =0;i<2;i++)
+        {
+            swapped = swap(originalNumber)
+        }
+    }
+    console.log("NewNum",newNumber);
+    console.log("NewNum",hash);
+    
+    // let newNumber = num.join("")
+    // console.log(newNumber);
+    // let minus = newNumber - originalNumber;
+    // if(minus > diff)
+    // {
+    //     diff = minus
+    // }
     
 }
 
 MaxSwap(2736)
+
+
+
+
+
+/*
+2736
+
+2,7,3,6
+7,2,3,6 = 7236
+first 2736 > 7236 no 7236 is bigger
+store in hash
+{7236}
+2736
+2376 > 2736 no so 2736 is big do not touch
+2736
+2763 > 2736 yes , is it bigger than 7236 no return 7236
+
+
+
+
+*/
